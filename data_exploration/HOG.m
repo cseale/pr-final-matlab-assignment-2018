fcef68e6188b0082f7ec150ed48ef0f22a43edc7
Gabriele Mazzola <gabriele.mazzola@techedgegroup.com>
2018-11-21 13:14:53 +0100

    test svm classifier with 25-fold validation: 0.0460 error

--- data_exploration/HOG.m
+++ data_exploration/HOG.m
@@ -27,16 +27,19 @@ title({'CellSize = [4 4]'; ['Length = ' num2str(length(hog4x4))]});
 
 
 
-%% Test svc classifier
+%% Test svc classifier 
 clear;
 close all;
 clc;
-prwaitbar off;
 
-digits = prnist([0:9],[1:20]);
+digits = prnist([0:9],[1:190]);
 
-preproc = im_box([],0,1)*im_resize([],[32 32])*im_box([],1,0);
+preproc = im_box([],0,1)*im_resize([],[32 32], 'bicubic')*im_box([],1,0);
 a = digits*preproc;
+
+
+features = im_features(a, a, 'all');
+
 
 % this is done once to get the length of the generated features
 cellSize = [4 4]; % [2 2] seems to yield the best result (still to verify) but it takes too much for the training
@@ -59,7 +62,7 @@ pr_a = prdataset(trainingFeatures, trainingLabels);
 w = svc(pr_a);
 
 %create test set
-test_digits = prnist([0:9],[21])*preproc;
+test_digits = prnist([0:9],[191:200])*preproc;
 show(test_digits);
 numTestImages = length(test_digits);
 testFeatures = zeros(numTestImages, hogFeatureSize);
@@ -71,7 +74,39 @@ end
 testLabels = getlabels(test_digits);
 pr_tst = prdataset(testFeatures, testLabels);
 
+
 res = w(pr_tst);
 
-testLabels'
-labeld(res)
+confmat(res);
+testc(pr_tst, w)
+
+
+%% TEST svc with HOG features -> error: 0.0460
+clear;
+close all;
+clc;
+prwaitbar on;
+
+digits = prnist([0:9], [1:100]);
+preproc = im_box([],0,1)*im_resize([],[32 32], 'bicubic')*im_box([],1,0);
+design_set = digits*preproc;
+
+
+% this is done once to get the length of the generated features
+cellSize = [8 8];
+[hog, vis] = extractHOGFeatures(data2im(design_set(1)),'CellSize',cellSize);
+hogFeatureSize = length(hog);
+
+% Loop over the trainingSet and extract HOG features from each image. A
+% similar procedure will be used to extract features from the testSet.
+numImages = length(design_set);
+features = zeros(numImages, hogFeatureSize);
+for i = 1:numImages
+    img = data2im(design_set(i));    
+    features(i, :) = extractHOGFeatures(img, 'CellSize', cellSize);  
+end
+
+labels = getlabels(digits);
+a = prdataset(features, labels);
+
+res1 = prcrossval(a,svc,25,1);
