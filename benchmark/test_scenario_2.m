d2bbf8348f4d7af3a66aa717821bd1153d044523
Gabriele Mazzola <gabriele.mazzola@techedgegroup.com>
2018-12-12 11:55:31 +0100

    complete benchmark tests

--- benchmark/test_scenario_2.m
+++ benchmark/test_scenario_2.m
@@ -2,7 +2,10 @@ clc;
 clear all;
 close all;
 
-trn_set = prnist([0:9], [31:40]);
+trn_set = prnist([0:9], [1:10]);
+trn_set = transform_images(trn_set);
+show(trn_set);
+pause;
 trn_feats = my_rep2(trn_set);
 w = svc(trn_feats);
 
