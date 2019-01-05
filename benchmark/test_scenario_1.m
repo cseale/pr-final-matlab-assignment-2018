54526a7c1cd27df1d3206491edc6a535d6ff101d
Gabriele Mazzola <gabriele.mazzola@techedgegroup.com>
2019-01-05 20:22:33 +0100

    add PCA plot and change scenario 1 benchmark

--- benchmark/test_scenario_1.m
+++ benchmark/test_scenario_1.m
@@ -4,17 +4,16 @@ close all;
 
 trn_set = prnist([0:9],[1:1000]);
 trn_feats = my_rep(trn_set);
-fprintf("Feature extracted.");
-% w = scalem([],'variance')*pcam([],0.9); %define
-% mapping = trn_feats*w; %mapping
-% new_trn = mapping(trn_feats); %mapped dataset
-% clsf = knnc(new_trn, 3); % trained classifier
-% 
-% err = nist_eval('my_rep', mapping*clsf, 100);
+fprintf("Features extracted.\n");
+w = scalem([],'variance')*pcam([],0.8); %define (0.8 is the best value)
+mapping = trn_feats*w; %mapping
+new_trn = mapping(trn_feats); %mapped dataset
+clsf = knnc(new_trn, 3); % trained classifier
+fprintf("Classifier trained.\n");
+err = nist_eval('my_rep', mapping*clsf, 100);
+fprintf("Done.\n");
 
-clsf = knnc(trn_feats, 3);
-fprintf("Classifier trained.");
-err = nist_eval('my_rep', clsf, 100);
-fprintf("Done.");
+% clsf = knnc(trn_feats, 3);
+% err = nist_eval('my_rep', clsf, 100);
 
 %save('trn_feats_4x4_bic', 'trn_feats')
